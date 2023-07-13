1bcac4ad79e0fc70010619face507343e5a9afcb
Cassidy Armstrong <carmstrong1@gmail.com>
2023-07-13 14:53:53 +0100

    first task comlete

--- targets.md
+++ targets.md
@@ -13,3 +13,27 @@ Be able to create an instance of a class (an object)
 Understand the difference between a class and an object
 Be able to add properties to class
 Be able to add methods to class
+
+SMART Targets.
+Task 1:
+Create a variable and assign it the value of “hello”
+Use an if statement to check if the variable is equal to hello, and if it is print”Hey there!”
+Create a dictionary with the keys of “brand” with the value of “nike” and the key of “type” and the value of “shoe”.
+Access and print out the value held under type from the dictionary just created
+Task 2:
+Create a list of numbers from 1 to 10
+Using a loop add two to each number
+Using a loop print out each number in the list
+	
+Task 3:
+Create a person class with the properties of  name (str), wallet (int). And a  shopping list (List) that starts off empty
+Write  methods to add and remove items from the shopping list
+Write a method called `print_shopping_list` to print out each item in the shopping list (using a loop)
+Task 4:
+Create an instance of the person class
+Add a string of “banana” to the shopping list of that person
+Add a string of “soap” to the shopping list of that person
+Print out the ‘name’ property of the person
+Call the person’s method to print out the shopping list.
+Task 5 :
+Try the lab with scaffold as a homework
---
+++ tasks.py
@@ -0,0 +1,37 @@
+# SMART Targets.
+# Task 1:
+# Create a variable and assign it the value of “hello”
+# Use an if statement to check if the variable is equal to hello, and if it is print”Hey there!”
+
+greeting = "hello"
+if greeting == "hello":
+    print("Hey there!")
+
+# Create a dictionary with the keys of “brand” with the value of “nike” and the key of “type” and the value of “shoe”.
+# Access and print out the value held under type from the dictionary just created
+shoe_dictionary = {"brand": "nike", "type": "shoe"}
+print(shoe_dictionary["type"])
+
+# Task 2:
+# Create a list of numbers from 1 to 10
+# Using a loop add two to each number
+# Using a loop print out each number in the list
+	
+# Task 3:
+# Create a person class with the properties of  name (str), wallet (int). And a  shopping list (List) that starts off empty
+# Write  methods to add and remove items from the shopping list
+# Write a method called `print_shopping_list` to print out each item in the shopping list (using a loop)
+# Task 4:
+# Create an instance of the person class
+# Add a string of “banana” to the shopping list of that person
+# Add a string of “soap” to the shopping list of that person
+# Print out the ‘name’ property of the person
+# Call the person’s method to print out the shopping list.
+# Task 5 :
+# Try the lab with scaffold as a homework
+greeting = "hello"
+if greeting == "hello":
+    print("Hey there!")
+
+
+ 

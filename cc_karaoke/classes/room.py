ffc675fdab7bd27ca20dbcf938e2cf733d402529
Cassidy Armstrong <carmstrong1@gmail.com>
2023-07-15 10:59:28 +0100

    created add_song method

--- cc_karaoke/classes/room.py
+++ cc_karaoke/classes/room.py
@@ -1,6 +1,18 @@
 class Room:
-    def __init__(self, name, capacity, till):
+    def __init__(self, name, capacity):
         self.name = name
         self.capcity = capacity
-        self.till = till
+        self.till = 0
         self.guests = []
+
+    def add_to_till(self, amount):
+        self.till += amount
+    
+    def check_in_guest(self, guest):
+        self.guests.append(guest)
+    
+    def check_out_guest(self, guest):
+        self.guests.remove(guest)
+    
+    def add_song(self, song):
+        self.songs.append(song)
